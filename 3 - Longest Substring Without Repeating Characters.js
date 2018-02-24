1b771b2e52d4c21ccf5422c02b06ff7018e75862
Zecheng Dong <zechengdong@Zechengs-MacBook-Pro.local>
2018-02-24 17:08:47 -0500

    longest substring without repeat letter

---
+++ 3 - Longest Substring Without Repeating Characters.js
@@ -0,0 +1,41 @@
+// Given a string, find the length of the longest substring without repeating characters.
+
+// Examples:
+
+// Given "abcabcbb", the answer is "abc", which the length is 3.
+
+// Given "bbbbb", the answer is "b", with the length of 1.
+
+// Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    if(s === null || s.length === 0){
+        return 0;
+    }
+
+    var currentLength = 0;
+    var longestLength = currentLength;
+    var hash = {};
+    var start = 0;
+
+    for(var i = start; i<s.length; i++) {
+        if(hash[s[i]] !== undefined && hash[s[i]] >= start) {
+           start = hash[s[i]] + 1;
+           currentLength = i - start;
+        } 
+        currentLength++;
+
+        if(currentLength > longestLength) {
+            longestLength = currentLength;
+        }
+
+        hash[s[i]] = i;
+    }
+
+    return longestLength;
+};
+
